d4670d7a65f7a0f89e5ad4a475efcde28230527c
Dina Shafeeva <di.shafeeva@yandex.ru>
2019-12-11 10:25:34 +0300

    first commit

---
+++ src/main/java/ru/dynasty/client/MainApp.java
@@ -0,0 +1,18 @@
+package ru.dynasty.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    public void start(Stage primaryStage) throws Exception {
+        String fxmlFile = "/MainPage.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        Parent root =   loader.load(getClass().getResourceAsStream(fxmlFile));
+        primaryStage.setTitle("JavaFX and Maven");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/ru/dynasty/client/MainPage.java
@@ -0,0 +1,4 @@
+package ru.dynasty.client;
+
+public class MainPage {
+}
