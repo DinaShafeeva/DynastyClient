4672fe9ad5e48d4880bbc7deda01d8aa91409e92
GribanovaNastya <hafizova-i@mail.ru>
2019-12-26 16:37:12 +0300

    Update ResponseHandler

--- src/main/java/ru/dynasty/client/Controllers/RoomController.java
+++ src/main/java/ru/dynasty/client/Controllers/RoomController.java
@@ -6,13 +6,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import ru.dynasty.client.ClientSocket;
 import ru.dynasty.client.Models.Card;
 import ru.dynasty.client.Models.CardPane;
 
@@ -20,29 +18,21 @@ import java.io.IOException;
 
 public class RoomController {
     @FXML
-    ImageView enemyHealth;
-    //должна меняться картинка здоровья противника
-    @FXML
-    ImageView myHealth;
-    //должна меняться картинка твоего здоровья
-    @FXML
-    Text damage;
-    //должен заполняться урон
-    @FXML
     Text yourName;
-    //твоя имя нужно получить
     @FXML
     Button end;
     @FXML
     Button send;
     @FXML
-    Button firstCard;
+    TextField oneMessage;
+
+    ClientSocket clientSocket;
 
     @FXML
-    static HBox hboxMyCard;
+    HBox hboxMyCard;
 
     @FXML
-    public static void method(Card card) {
+    public static void drawCard(Card card, HBox hboxMyCard) {
         CardPane cardPane = new CardPane(card);
         hboxMyCard.getChildren().addAll(cardPane);
     }
@@ -55,23 +45,12 @@ public class RoomController {
         stage.setScene(new Scene(root));
         stage.showAndWait();
     }
+
+
     @FXML
     public void onClickSend(ActionEvent actionEvent) throws IOException {
-        //происходит отправка сообщения
+        String s = oneMessage.getText();
+        clientSocket.sendMessage(yourName.getText() + ": " + s);
     }
-    @FXML
-    public void onClickCard(ActionEvent actionEvent, Button card, Text name, Text power, Text dif) throws IOException {
-        if(nameCard1.equals(" ")){
-            nameCard1.setText(name.getText());
-            powerCard1.setText(power.getText());
-            difCard1.setText(dif.getText());
-        } else {
-            nameCard2.setText(name.getText());
-            powerCard2.setText(power.getText());
-            difCard2.setText(dif.getText());
-        }
-        name.setText(" ");
-        power.setText(" ");
-        dif.setText(" ");
-    }
+
 }
--- src/main/java/ru/dynasty/client/Models/CardPane.java
+++ src/main/java/ru/dynasty/client/Models/CardPane.java
@@ -1,5 +1,6 @@
 package ru.dynasty.client.Models;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -9,13 +10,16 @@ public class CardPane extends Pane {
     private Card card;
 
     public CardPane(Card card) {
-
-        Image img = new Image("src/main/resources/Images/health0.png");
+        Button button = new Button();
+        button.setPrefWidth(108);
+        button.setPrefHeight(176);
+        button.setStyle("-fx-background-color :  000000");
+        Image img = new Image("src/main/resources/Images/Cards.png");
         ImageView imageView = new ImageView();
         imageView.setImage(img);
-        Label label1 = new Label("1");
-        Label label2 = new Label("1");
-        Label label3 = new Label("1");
-        getChildren().addAll(imageView,label1,label2,label3);
+        Label name = new Label("");
+        Label power = new Label("Power ");
+        Label dif = new Label("Dif ");
+        getChildren().addAll(imageView,name, power, dif, button);
     }
 }
